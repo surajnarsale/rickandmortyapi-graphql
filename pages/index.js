a125818bb4ef3075f0d782843bd01862d4460e04
Suraj Narsale <surajsnarsale@gmail.com>
2021-12-01 09:23:21 +0530

    feat: display graphql data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
 import Head from 'next/head';
+import { gql } from '@apollo/client';
+import client from '../apollo-client';
 
-export default function Home() {
+export default function Home({ characters }) {
+	console.log(characters);
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
-			<h1>Welcome</h1>
+		<div>
+			{characters.map((character) => (
+				<h1 key={character.name}>{character.name}</h1>
+			))}
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	const { data } = await client.query({
+		query: gql`
+			query {
+				characters {
+					results {
+						id
+						name
+						image
+					}
+				}
+			}
+		`,
+	});
+
+	return {
+		props: {
+			characters: data.characters.results.slice(0, 4),
+		},
+	};
 }
